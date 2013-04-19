d860859db8ce5d2531b7d568122d7c99a8f93dfc
Selwin Ong <selwin.ong@gmail.com>
2013-04-19 16:04:58 +0700

    Bump version to 0.2.0.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='django-cached_authentication_middleware',
-    version='0.1.1',
+    version='0.2.0',
     author='Selwin Ong',
     author_email='selwin.ong@gmail.com',
     packages=['cached_auth'],
